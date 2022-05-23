5f5d5f09e4d8c4a915dfe8e46f9af705d0778592
ANTONOVA <89146019257@mail.ru>
2022-05-23 17:41:30 +0900

    Update AuthTest

--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -1,6 +1,7 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataGenerator;
@@ -17,6 +18,7 @@ public class AuthTest {
     @BeforeEach
     void setUp() {
         open("http://localhost:9999");
+        Configuration.holdBrowserOpen=true;
     }
 
     @Test
